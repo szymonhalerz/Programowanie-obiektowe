338c446301ef6a39972dec2e24f68f9faa472dff
Szymon <74181048+szymonhalerz@users.noreply.github.com>
2026-01-25 18:11:44 +0100

    Add files via upload
    Lab_1 - Zadanie_3

---
+++ Zadanie3/Zadanie3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Zadanie3
+{
+    //3.Napisz program wyświetlający liczby od 20-0, z wyłączeniem liczb{2,6,9,15,19}. Do realizacji
+    //zadania wyłączenia użyj instrukcji continue;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WypiszLiczby();
+        }
+
+        static void WypiszLiczby()
+        {
+            for (int i = 20; i >= 0; i--)
+            {
+                // {2,6,9,15,19}
+                if (i == 2 || i == 6 || i == 9 || i == 15 || i == 19)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
