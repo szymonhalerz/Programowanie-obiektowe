b7d800f953d8f6cb85b9eb795f96483eaac3b7bf
Szymon <74181048+szymonhalerz@users.noreply.github.com>
2026-01-25 18:12:45 +0100

    Add files via upload
    Lab_1 - Zadanie_5

---
+++ Zadanie5/Zadanie5/Program.cs
@@ -0,0 +1,67 @@
+﻿//5.Napisz program umożliwiający wprowadzanie n liczb oraz sortujący te liczby metodą
+//bąbelkową lub wstawiania. Wyniki wyświetlaj na konsoli.
+
+using System;
+
+namespace Zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] tablica = WczytajLiczby();
+
+            Console.WriteLine("\nTablica przed posortowaniem:");
+            WypiszTablice(tablica);
+
+            SortujBabelkowo(tablica);
+
+            Console.WriteLine("\nTablica po posortowaniu (rosnąco):");
+            WypiszTablice(tablica);
+
+            Console.ReadKey();
+        }
+
+        static double[] WczytajLiczby()
+        {
+            Console.Write("Podaj ile liczb chcesz wprowadzić (n): ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            double[] liczby = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Podaj liczbę {i + 1}: ");
+                liczby[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            return liczby;
+        }
+
+        static void SortujBabelkowo(double[] tab)
+        {
+            int n = tab.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (tab[j] > tab[j + 1])
+                    {
+                        double temp = tab[j];
+                        tab[j] = tab[j + 1];
+                        tab[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        static void WypiszTablice(double[] tab)
+        {
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.Write(tab[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
