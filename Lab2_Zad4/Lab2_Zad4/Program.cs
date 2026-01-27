946b2c312fccb373fcea074fa978f1c248341bf2
Szymon <74181048+szymonhalerz@users.noreply.github.com>
2026-01-27 19:07:12 +0100

    Add files via upload
    Lab2_Zad4

---
+++ Lab2_Zad4/Lab2_Zad4/Program.cs
@@ -0,0 +1,50 @@
+﻿//Zadanie 4.
+//Stwórz klasę Licz z:
+//• publicznym polem value przechowującym wartość liczbową.
+//• metodą Dodaj przyjmującą jeden parametr i dodającą przekazaną wartość do wartości
+//trzymanej w polu value.
+//• analogiczną operację odejmij
+//W Main utwórz kilka obiektów klasy Licz i wykonaj różne operacje.
+//Do klasy Licz dodaj konstruktor z jednym parametrem - który inicjuje pole wartość na liczbę przekazaną
+//w parametrze.
+//Zmień widoczność pola na private i dodaj funkcję wypisującą stan obiektu (pole value)
+
+using System;
+
+namespace Lab2_Zad4
+{
+    static class Program
+    {
+        public class Licz
+        {
+            private double value;
+
+            public void Dodaj(double parametr)
+            {
+                this.value += parametr;
+            }
+
+            public void Odejmij(double parametr)
+            {
+                this.value -= parametr;
+            }
+
+            public Licz(double x)
+            {
+                this.value = x;
+            }
+
+            public void WypiszStan() 
+            {
+                Console.WriteLine($"Aktualna wartość: {this.value}.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Licz obiekt1 = new Licz(3);
+            obiekt1.Dodaj(2);
+            obiekt1.Odejmij(1);
+            obiekt1.WypiszStan();
+        }
+    }
+}
